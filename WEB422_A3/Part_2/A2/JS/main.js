4ff42643ae0e1b79da653e271441ce99f9c7d453
unknown <IvyLeano@outlook.com>
2019-06-15 20:38:20 -0400

    Part 2 complete

--- WEB422_A3/Part_2/A2/JS/main.js
+++ WEB422_A3/Part_2/A2/JS/main.js
@@ -1,5 +1,5 @@
 /********************************************************************************* *
-WEB422 – Assignment 2
+WEB422 – Assignment 3
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. * No part
 of this assignment has been copied manually or electronically from any other source * (including web
 sites) or distributed to other students.
@@ -12,53 +12,114 @@ sites) or distributed to other students.
 //Note: $.ajax: https://www.w3schools.com/jquery/ajax_ajax.asp
 //              https://api.jquery.com/jquery.get/
 
-$(document).ready(function () {
-  console.log("Document Ready");
-  let employeesModel = [];
-  intializeEmployeesModel();
+ //object viewModel, knockout observable properties/values
+ var viewModel = {
+  teams: ko.observable([]),
+  employees: ko.observable([]),
+  projects: ko.observable([])
+};
 
-  $("#employee-search").on("keyup", function () {
-    var filtered = getFilteredEmployeesModel(this.value);
-    refreshEmployeeRows(filtered);
-  });
-  
-  $(document.body).on('click', '.body-row', function () {
-    var emp = getEmployeeModelById($(this).attr("data-id"));
-    var isEmpEmpty = emp == null;
-    var template;
-    var content;
-    if (!isEmpEmpty) {
-      emp.HireDate = moment(emp.HireDate).format('LL');
-      template = _.template(
-        '<strong>Address:</strong> <%- employee.AddressStreet %> <%- employee.AddressCity %>, <%- employee.AddressState %>. <%- employee.AddressZip %></br>' +
-        '<strong>Phone Number:</strong> <%- employee.PhoneNum %> ext: <%- employee.Extension %></br>' +
-        '<strong>Hire Date:</strong> <%- employee.HireDate %>'
+//A3: updated to include initializeTeams() and initializeProjects()
+$(document).ready(function() {
+  console.log("Document ready");
+  initializeTeams()
+      .then(
+        intializeEmployeesModel
+      )
+      .then(
+        initializeProjects
+      )
+      .then(function(reject,resolve) {
+              ko.applyBindings(viewModel, $("body")[0]);
+              $("select.multiple").multipleSelect({filter:true});
+              $("select.single").multipleSelect({single:true, filter:true});
+          }
       );
-      content = template({ 'employee': emp });
-      showGenericModal(emp.FirstName + ' ' + emp.LastName, content);
-    }
-  });
 });
-
-function intializeEmployeesModel() {
-  $.ajax({
-    url: "https://fierce-ridge-73424.herokuapp.com/employees",
-    type: "GET",
-    contentType: "application/json"
-  }).done(function (data) {
-    employeesModel = data;
-    refreshEmployeeRows(employeesModel);
-  }).fail(function (err) {
-    showGenericModal('Error', 'Unable to get Employees');
+//A3: initialize projects function added
+function initializeProjects() {
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: "https://fierce-ridge-73424.herokuapp.com/projects",
+      type: "GET",
+      contentType: "application/json"
+    }).done(function (data) {
+      viewModel.projects = ko.mapping.fromJS(data);
+      //refreshEmployeeRows(employeesModel);
+      resolve();
+    }).fail(function (err) {
+      // showGenericModal('Error', 'Unable to get Employees');
+      reject("Error loading the project data");
+    });
+  });
+}
+//A3: initialize teams function added
+function initializeTeams() {
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: "https://fierce-ridge-73424.herokuapp.com/teams-raw",
+      type: "GET",
+      contentType: "application/json"
+    }).done(function (data) {
+    //  console.log(json.parse(data))
+    viewModel.teams  = ko.mapping.fromJS(data);
+   
+      //refreshEmployeeRows(employeesModel);
+      resolve();
+    }).fail(function (err) {
+      // showGenericModal('Error', 'Unable to get Employees');
+      reject("Error loading the team data");
+    });
   });
 }
 
+//A3: updated to return a promise and populate "employees" property
+function intializeEmployeesModel() {
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: "https://fierce-ridge-73424.herokuapp.com/employees",
+      type: "GET",
+      contentType: "application/json"
+    }).done(function (data) {
+      // employeesModel = data;
+      //refreshEmployeeRows(employeesModel);
+      viewModel.employees = ko.mapping.fromJS(data);
+      resolve();
+    }).fail(function (err) {
+      // showGenericModal('Error', 'Unable to get Employees');
+      reject("Error loading the employee data");
+    });
+  });
+}
+//A3: same function in A2, no updates
 function showGenericModal(title, message) {
-  $("#genericModal .modal-title").append(title);
+  $("#genericModal .modal-title").empty().append(title);
   $("#genericModal .modal-body").append(message);
   $("#genericModal").modal('show');
 };
 
+//A3: save team functions
+function saveTeam(){
+  var currentTeam = this;
+  $.ajax({
+    url: "https://fierce-ridge-73424.herokuapp.com/team/" + currentTeam._id(),
+    type: "Put",
+    data: JSON.stringify(
+      {
+        Projects: currentTeam.Projects(),
+        Employees: currentTeam.Employees(),
+        TeamLead: currentTeam.TeamLead()
+      }
+    ),
+    contentType: "application/json"
+  }).done(function(data){
+    showGenericModal("Success", currentTeam.TeamName() + " Updated Successfully");
+  }).fail(function(error){
+    showGenericModal("Error", "Error updating the team information." );
+  });
+
+}
+/*
 function refreshEmployeeRows(employees) {
   $("#employees-table").empty();
   var lodashTemplate = _.template('<% _.forEach(employees,function(employee){%>' +
@@ -75,7 +136,7 @@ function refreshEmployeeRows(employees) {
 function getFilteredEmployeesModel(filterString) {
   var filteredEmployeesModel = _.filter(employeesModel, function (emp) {
     var firstName = emp.FirstName.toLowerCase().includes(filterString.toLowerCase());
-    var lastName =  emp.LastName.toLowerCase().includes(filterString.toLowerCase());
+    var lastName = emp.LastName.toLowerCase().includes(filterString.toLowerCase());
     var position = emp.Position.PositionName.toLowerCase().includes(filterString.toLowerCase());
     return (firstName || lastName || position) ? true : false;
   });
@@ -85,14 +146,12 @@ function getFilteredEmployeesModel(filterString) {
 function getEmployeeModelById(id) {
   var data = null;
   $.grep(employeesModel, function (x) {
-   if(x._id == id) {
-     data = _.cloneDeep(x);
-   } 
+    if (x._id == id) {
+      data = _.cloneDeep(x);
+    }
   });
   return data;
 }
-
-
-
+*/
 
 
